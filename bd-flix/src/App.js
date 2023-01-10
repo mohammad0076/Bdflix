d37ee3be1792391c3bc956617fb27b70028ba96b
Ashim-mollick <ashimmollick46@gmail.com>
2023-01-10 16:00:33 +0600

    ashim

--- bd-flix/src/App.js
+++ bd-flix/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Ashim
+          Learn React
         </a>
       </header>
     </div>
