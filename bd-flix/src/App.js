d2abf137f93f144b095620493f6fe261e722dca7
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2023-01-10 15:45:07 +0600

    added my name

--- bd-flix/src/App.js
+++ bd-flix/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Avijit Sarkar
         </a>
       </header>
     </div>
