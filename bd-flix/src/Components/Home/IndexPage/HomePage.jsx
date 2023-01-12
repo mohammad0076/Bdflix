a65a3370dba6e4b069dbe97de71cdcb3d7a2b9b5
emon <emondas457@gmail.com>
2023-01-13 01:27:10 +0600

    add new bottom bar

--- bd-flix/src/Components/Home/IndexPage/HomePage.jsx
+++ bd-flix/src/Components/Home/IndexPage/HomePage.jsx
@@ -7,11 +7,6 @@ const HomePage = () => {
     return (
         <div>
             <Slider></Slider>
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 2ecda827d8f9d8b87e223ae76be438c89fffe882
             <MostPopular></MostPopular>
             <MoviesForYou></MoviesForYou>
         </div>
