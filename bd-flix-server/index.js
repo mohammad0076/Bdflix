d806fa5a0b4662fb6c1f069da8d0b960eeb44b91
emon <emondas457@gmail.com>
2023-01-24 14:05:12 +0600

    add post movie api

--- bd-flix-server/index.js
+++ bd-flix-server/index.js
@@ -11,12 +11,6 @@ app.get('/', (req, res) => {
     res.send('hello');
 })
 
-
-
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ac1kfa5.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -25,9 +19,16 @@ async function run() {
         const MostPopularMoviesCategoriCollection = client.db("bdFlix").collection("MostPopularMovie");
         const MoviesForYouCategoriCollection = client.db("bdFlix").collection("MoviesForYou");
         const ComediesCollection = client.db("bdFlix").collection("comedies");
+        const allMoviesCollection = client.db("bdFlix").collection("allmovies");
 
         app.get('/mostPopularMovies', async (req, res) => {
             const result = await MostPopularMoviesCategoriCollection.find({}).toArray();
+            res.send(result);
+        })
+
+        app.post('/allmovies', async (req, res) => {
+            const allmovies = req.body;
+            const result = await allMoviesCollection.insertOne(allmovies);
             res.send(result);
         })
 
